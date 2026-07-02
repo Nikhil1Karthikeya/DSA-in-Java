e340154c6003a3ee04cd3505cd164c460a9aa36f
Nikhil1Karthikeya <nikhilkarthik312@gmail.com>
2026-07-02 15:47:49 +0530

    Solved LeetCode 20 - Valid Parentheses

---
+++ Stack/LC20_valid_parentheses.java
@@ -0,0 +1,32 @@
+package Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+       char ch;
+
+       for(int i=0;i<s.length();i++){
+    ch=s.charAt(i);
+    if(ch=='('||ch=='{'||ch=='['){
+        stack.push(ch);
+    }
+    else{
+    if(stack.isEmpty()){
+        return false;
+    }
+    if(ch==')'&&stack.peek()=='('||
+    ch=='}'&&stack.peek()=='{'||
+    ch==']'&&stack.peek()=='['){
+        stack.pop();
+    }
+    else{
+        return false;
+    }
+    }
+       } 
+       if(stack.isEmpty()){
+        return true;
+       }
+       return false;
+    }
+}
