ee9ce0c9134eb8048b7a16decd07ed07a07fc89f
Nikhil1Karthikeya <nikhilkarthik312@gmail.com>
2026-07-24 17:47:33 +0530

    Solve LeetCode 1672: Richest Customer Wealth

---
+++ Arrays/LC1672_richest_customer_wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum;
+        int maxsum=0;
+        for(int i=0;i<accounts.length;i++){
+            sum=0;
+        for(int j=0;j<accounts[i].length;j++){
+sum=sum+accounts[i][j];
+if(sum>maxsum){
+    maxsum=sum;
+}
+        }
+        }
+        return maxsum;
+    }
+}
