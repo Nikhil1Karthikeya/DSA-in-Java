8f43dea5fa44ce3db0fe37d98cbf901183fa6331
Nikhil1Karthikeya <nikhilkarthik312@gmail.com>
2026-07-26 10:13:54 +0530

    Solve LeetCode 167 - Two Pointers

---
+++ Arrays/LC167_two_sumii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int [] ans=new int[2];
+        int n=numbers.length;
+        int left=0,right=n-1;
+        while(left<right){
+        if(numbers[left]+numbers[right]==target){
+            ans[0]=left+1;
+            ans[1]=right+1;
+            return ans;
+        }
+        else if(numbers[left]+numbers[right]>target){
+            right--;
+        }
+        else{
+            left++;
+        }
+        }
+        return ans;
+    }
+}
